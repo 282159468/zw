697e05806660767f3be49c9c88af5b5b042990cd
282159468 <282159468@qq.com>
2021-07-28 07:27:29 +0800

    docs: 卓有成效的管理者-前言

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'zw',
   mode: 'site',
+
+  mfsu: {},
+  webpack5: {},
+  dynamicImport: {},
   // more config: https://d.umijs.org/config
 });

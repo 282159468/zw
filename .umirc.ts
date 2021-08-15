3a8a590ebcb97b956911a884ee2a3733f8026bd5
282159468 <282159468@qq.com>
2021-08-15 19:01:38 +0800

    chore: update gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,11 @@ export default defineConfig({
   title: 'zw',
   mode: 'site',
   exportStatic: {},
-  mfsu: {},
+  // mfsu: {},
   webpack5: {},
+  targets: {
+    chrome: '92',
+  },
   dynamicImport: {},
   // more config: https://d.umijs.org/config
 });
